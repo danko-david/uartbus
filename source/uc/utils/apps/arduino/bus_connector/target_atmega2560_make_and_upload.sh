85fa52eebb8af0ffa246547ed05f81d98a7f66bc
Dankó Dávid <info@dankodavid.hu>
2019-03-09 02:01:20 +0100

    now with selectable baud rate

--- source/uc/utils/apps/arduino/bus_connector/target_atmega2560_make_and_upload.sh
+++ source/uc/utils/apps/arduino/bus_connector/target_atmega2560_make_and_upload.sh
@@ -2,7 +2,7 @@
 #usage: target_atmega2560_make_and_upload.sh $ttyUSB$num
 #example target_atmega2560_make_and_upload.sh 0 //this upload to adruino attached to /dev/ttyUSB0
 make clean
-MCU=atmega2560 F_CPU=16000000 EXT_FLAGS="-DBAUD=19200 -DPC_SERIAL0 -DBUS_SERIAL3" make
+MCU=atmega2560 F_CPU=16000000 EXT_FLAGS="-DBAUD=$1 -DPC_SERIAL0 -DBUS_SERIAL3" make
 
-avrdude -p atmega2560 -b115200 -c arduino -P /dev/ttyUSB$1 -cwiring -D -Uflash:w:uartbus_connector.cpp.hex:i
+avrdude -p atmega2560 -b115200 -c arduino -P /dev/ttyUSB$2 -cwiring -D -Uflash:w:uartbus_connector.cpp.hex:i
 
