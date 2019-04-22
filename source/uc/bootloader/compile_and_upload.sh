d542f392dacd48dd47eebee350ed46fa193043aa
Dankó Dávid <info@dankodavid.hu>
2019-04-22 18:29:33 +0200

    How to break runtime without get any notice, fix and explanation

--- source/uc/bootloader/compile_and_upload.sh
+++ source/uc/bootloader/compile_and_upload.sh
@@ -22,13 +22,17 @@ avr-gcc -o ubb.o ub_bootloader.cpp ../bus/lib/common/ub.cpp ../utils/lib/rpc/rpc
 	-DHOST_TABLE_ADDRESS=0x1fe0\
 	-DAPP_START_ADDRESS=0x2000\
 	-DAPP_CHECKSUM=0\
-	-Wl,--defsym=__stack=0x800300\
-	-Wl,--section-start=.data=0x800302\
-	-Wl,-Tdata,0x800302\
-	-Wl,-Tbss,0x800346
+	-Wl,--defsym=__stack=0x800200\
+	-Wl,--section-start=.data=0x800202\
+	-Wl,-Tbss,0x800300
 
 
 #TODO calculate data, bss, stack adresses
+#WARNING: bss, data and stack might collide and NOTHING NOTICES THAT!
+# Before you give up your life and get self suicide, beacuse of the random weird behaviors
+# recalculate manually these boundary values.
+# highest value: 0x800000 + RAMEND (atmega328: 0x8FF)
+# RAMEND from /usr/lib/avr/include/avr/iom328p.h
 
 avr-objcopy -O ihex -R .eeprom ubb.o ubb.hex
 #wc ubb.hex
