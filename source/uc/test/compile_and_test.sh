6ea940ccd21e47088317564836b6e2c67ae4d66b
Dankó Dávid <info@dankodavid.hu>
2019-05-20 17:17:14 +0200

    fix rename in script

--- source/uc/test/compile_and_test.sh
+++ source/uc/test/compile_and_test.sh
@@ -4,7 +4,7 @@ rm test.so
 
 set -e
 
-gcc -DUB_TEST -fPIC -shared -g -rdynamic -llct -I../bus/lib/common -I../commons -I../utils/lib/rpc test.c -o test.so
+gcc -DUB_TEST -fPIC -shared -g -rdynamic -llct -I../bus/lib/common -I../commons -I../utils/lib/rpc test_uartbus.c -o test_uartbus.so
 
-lct ./test.so
+lct ./test_uartbus.so
 
