ed1457c87197c4209148f832a736b6c97b83d91b
Dankó Dávid <info@dankodavid.hu>
2019-08-17 03:03:19 +0200

    Feature to construct reflectable tree of RPC nodes and functions. And external software can access and get known about the RPC tree and the functions in there, the signature and retval of the function and for humans a small description of the function.
    This gonna be a resource demanding solution, that's why wasn't part of the basic
    functionality and codebase
    
    Im still in progress to get inspiration for the desing of this by working on an
    implementation Java SNMP server, zabbix and autodiscovery, information providing mechanism.

--- source/uc/utils/lib/rpc/rpc_node.h
+++ source/uc/utils/lib/rpc/rpc_node.h
@@ -17,9 +17,50 @@
 #ifndef RPC_NODE_H_
 #define RPC_NODE_H_
 
+#include "rpc.h";
+
+enum rpc_modifiers
+{
+	rpc_modifier_function = 1,
+};
+
+struct rpc_node
+{
+	uint8_t ns;
+	
+	uint8_t modifiers;
+	
+	char* name;
+
+	union
+	{
+		bool (rpc_function*)(struct rpc_node*, struct rpc_request*);
+		struct rpc_node** sub_nodes;
+	}
+	
+	void* user_data;
+	//TODO respone function hook
+};
 
 
+bool rpc_dispatch_request(struct rpc_node* root, struct rpc_request*);
 
+bool rpc_dispatch_response(struct rpc_node* root, struct rpc_request*);
+
+bool rpc_dispatch_reflect_request(struct rpc_node* root, struct rpc_request*);
+
+
+/**
+ * Dispatches the request. If the first namespace is:
+ *	- 255 we dispatch as a reflection request,
+ *	- 0 we dispatch as a response packet (the registered response function
+ *  	handles the packet)
+ *  - otherwise dispatch as a request packet, the registered handler function
+ *		will handle the packet.
+ */
+bool rpc_dispatch_root(struct rpc_node* root, struct rpc_request*);
+
+struct rpc_node* rpc_create_node(int ns, const char* name, struct rpc_node** sub_nodes);
 
 
 #endif
