0708ec3bfdbc70c8972d7ee7dfbffb0c97b49ccb
Dankó Dávid <info@dankodavid.hu>
2019-11-13 02:38:54 +0100

    - yet another fix to machine agnostic build - add indirect dependencies

--- scripts/deb_install_deps.sh
+++ scripts/deb_install_deps.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get install socat git gcc-avr arduino maven openjdk-8-j{re,dk}
+sudo apt-get install socat git gcc-avr arduino maven openjdk-8-j{re,dk} libpcre3-dev libpth-dev
--- source/uc/test/CI_hooks_tests.sh
+++ source/uc/test/CI_hooks_tests.sh
@@ -15,8 +15,8 @@ case $1 in
 	;;
 
 	build)
-		gcc -DUB_TEST -fPIC -shared -g -rdynamic -I../bus/lib/common -I../commons -I../utils/lib/rpc test_uartbus.c -o test_uartbus.so
-		gcc -DUB_TEST -DUBH_TEST -fPIC -shared -g -rdynamic -I../bus/lib/common -I../commons -I../utils/lib/rpc ../utils/lib/rpc/rpc.c test_ubh.c -o test_ubh.so
+		gcc -DUB_TEST -fPIC -shared -g -rdynamic -I../bus/lib/common -I../commons -I../utils/lib/rpc -I../../../WD/lazyctest/src/c test_uartbus.c -o test_uartbus.so
+		gcc -DUB_TEST -DUBH_TEST -fPIC -shared -g -rdynamic -I../bus/lib/common -I../commons -I../utils/lib/rpc ../utils/lib/rpc/rpc.c -I../../../WD/lazyctest/src/c test_ubh.c -o test_ubh.so
 	;;
 
 	test)
