f5e11a200508ae2312da2a00d818263ba32e6636
Dankó Dávid <info@dankodavid.hu>
2019-12-02 16:13:11 +0100

    C RPC in progress

--- source/uc/utils/lib/rpc/rpc_node.c
+++ source/uc/utils/lib/rpc/rpc_node.c
@@ -1,6 +1,173 @@
 
 #include "rpc_node.h"
+#include <stdlib.h>
+
+struct rpc_function_parameter* rpc_il_create_parameter
+(
+	enum rpc_data_type value_type,
+	uint8_t data_size,
+	const char* param_name,
+	const char* param_description
+)
+{
+	struct rpc_function_parameter* ret = calloc(1, sizeof(struct rpc_function_parameter));
+	ret->data_type = value_type;
+	ret->data_size_bytes = data_size;
+	ret->name = param_name;
+	ret->descr = param_description;
+	return ret;
+}
+
+struct rpc_node* rpc_il_create_ns_node
+(
+	int ns,
+	const char* name,
+	uint8_t subnodes,
+	/*struct rpc_node* */ ...
+)
+{
+	struct rpc_node* ret = calloc(1, sizeof(struct rpc_node));
+	ret->ns = ns,
+	ret->name = name;
+	ret->modifiers = rpc_modifier_namespace;
+	ret->name = name;
+	ret->sub_nodes = malloc((subnodes+1)*sizeof(void*));
+	ret->sub_nodes[subnodes] = NULL;
+
+	va_list v;
+	va_start(v, subnodes);
+	for(uint8_t i=0;i<subnodes;++i)
+	{
+		ret->sub_nodes[i] = va_arg(v, struct rpc_node*);
+	}
+	va_end(v);
+
+	return ret;
+}
+
+struct rpc_node* rpc_il_create_function_node
+(
+	int ns,
+	const char* name,
+	void (*rpc_function)(struct rpc_request*),
+	uint8_t return_params,
+	uint8_t request_params,
+	/* struct rpc_function_parameter* */...
+)
+{
+	struct rpc_node* ret = calloc(1, sizeof(struct rpc_node));
+	ret->ns = ns,
+	ret->name = name;
+	ret->modifiers = rpc_modifier_function;
+	ret->name = name;
+
+	if(0 != return_params + request_params)
+	{
+		ret->func_descriptor = calloc(1, sizeof(struct rpc_function_descriptor));
+		va_list v;
+		va_start(v, request_params);
+		if(0 != return_params)
+		{
+			ret->func_descriptor->return_values = malloc((return_params+1)*sizeof(void));
+			ret->func_descriptor->return_values[return_params] = NULL;
+			for(uint8_t i=0;i<return_params;++i)
+			{
+				ret->func_descriptor->return_values[i] = va_arg(v, struct rpc_function_parameter*);
+			}
+		}
+
+		if(0 != request_params)
+		{
+			ret->func_descriptor->parameters = malloc((request_params+1)*sizeof(void));
+			ret->func_descriptor->parameters[request_params] = NULL;
+			for(uint8_t i=0;i<request_params;++i)
+			{
+				ret->func_descriptor->parameters[i] = va_arg(v, struct rpc_function_parameter*);
+			}
+			va_end(v);
+		}
+	}
+
+	return ret;
+}
+
+struct rpc_node* rpc_get_ns(struct rpc_node* node, uint8_t ns)
+{
+	if(NULL != node->sub_nodes)
+	{
+		for(uint16_t i = 0;i<256;++i)
+		{
+			struct rpc_node* at = node->sub_nodes[i];
+			if(NULL == at)
+			{
+				return NULL;
+			}
+			else if(ns == at->ns)
+			{
+				return at;
+			}
+		}
+	}
+
+	return NULL;
+}
+
+/*
 
 
 
 
+
+*/
+
+
+bool rpc_dispatch_with_mode
+(
+	struct rpc_node* node,
+	struct rpc_request* req,
+	enum rpc_dispatch_mode mode
+)
+{
+	//walk the namespaces
+
+	struct rpc_node* crnt = node;
+	//while(node->modifiers & rpc_modifier_namespace)
+	if(node->modifiers & rpc_modifier_namespace)
+	{
+		if()
+		{
+
+		}
+	}
+
+}
+
+bool rpc_dispatch_root(struct rpc_node* root, struct rpc_request* req)
+{
+	if(req->procPtr >= req->size)
+	{
+		return false;
+	}
+
+	int ns = req->payload[req->procPtr];
+
+	switch(ns)
+	{
+	case 0:
+		//dispatch response
+		++req->procPtr;
+		return rpc_dispatch_with_mode(root, req, rpc_dispatch_mode_response);
+
+
+	case 255:
+		//dispatch reflect request
+
+		//TODO handle reflect other way
+		++req->procPtr;
+		return rpc_dispatch_with_mode(root, req, rpc_dispatch_mode_reflect);
+
+	default:
+		//dispatch request
+		return rpc_dispatch_with_mode(root, req, rpc_dispatch_mode_request);
+	}
+}
--- source/uc/utils/lib/rpc/rpc_node.h
+++ source/uc/utils/lib/rpc/rpc_node.h
@@ -8,7 +8,6 @@
  * 	- dispatch the responses
  * 	- provide functionality to discover the tree
  *	- ability to describe function and return value parameters
- *	- subscribe to group ids
  *
  *
  *
@@ -17,11 +16,49 @@
 #ifndef RPC_NODE_H_
 #define RPC_NODE_H_
 
-#include "rpc.h";
+#include "rpc.h"
 
 enum rpc_modifiers
 {
 	rpc_modifier_function = 1,
+	rpc_modifier_namespace = 2,
+
+	rpc_modifier_advanced_handler = 4,
+};
+
+enum rpc_data_type
+{
+	/**
+	 * When a function ever returns like hardware_reset();
+	 * */
+	rpc_data_type_none,
+	rpc_data_type_void,
+	rpc_data_type_errno,
+	rpc_data_type_int,
+	rpc_data_type_uint,
+	rpc_data_type_vint,
+	rpc_data_type_vuint,
+	rpc_data_type_string,
+	rpc_data_type_float,
+	rpc_data_type_blob,//0 escaped byte sequence
+
+	//TODO object/array?
+	//TODO specify length {u,}int{8,16,32,64}_t
+};
+
+struct rpc_function_parameter
+{
+	enum rpc_data_type data_type;
+	uint16_t data_size_bytes;
+	const char* name;
+	const char* descr;
+	const char* meta;
+};
+
+struct rpc_function_descriptor
+{
+	struct rpc_function_parameter** return_values;
+	struct rpc_function_parameter** parameters;
 };
 
 struct rpc_node
@@ -30,16 +67,23 @@ struct rpc_node
 	
 	uint8_t modifiers;
 	
-	char* name;
+	const char* name;
+
+	const char* description;
+
+	const char* meta;
+
+	struct rpc_function_descriptor* func_descriptor;
 
 	union
 	{
-		bool (rpc_function*)(struct rpc_node*, struct rpc_request*);
+		void (*rpc_function)(struct rpc_request*);
+		bool (*rpc_ext_function)(struct rpc_node*, struct rpc_request*, uint8_t req_mode);
 		struct rpc_node** sub_nodes;
-	}
+	};
 	
 	void* user_data;
-	//TODO respone function hook
+	//TODO response function hook
 };
 
 
@@ -58,9 +102,42 @@ bool rpc_dispatch_reflect_request(struct rpc_node* root, struct rpc_request*);
  *  - otherwise dispatch as a request packet, the registered handler function
  *		will handle the packet.
  */
-bool rpc_dispatch_root(struct rpc_node* root, struct rpc_request*);
+bool rpc_dispatch_root(struct rpc_node*, struct rpc_request*);
 
-struct rpc_node* rpc_create_node(int ns, const char* name, struct rpc_node** sub_nodes);
+//struct rpc_node* rpc_create_namespace_node(int ns, const char* name, struct rpc_node** sub_nodes);
 
+struct rpc_node* rpc_il_create_ns_node(int ns, const char* name, uint8_t subnodes, /*struct rpc_node* */ ...);
+
+struct rpc_node* rpc_il_create_function_node
+(
+	int ns,
+	const char* name,
+	void (*rpc_function)(struct rpc_request*),
+	uint8_t return_params,
+	uint8_t request_params,
+	/* struct rpc_function_parameter* */...
+);
+
+struct rpc_function_parameter* rpc_il_create_parameter
+(
+	enum rpc_data_type value_type,
+	uint8_t data_size,
+	const char* param_name,
+	const char* param_description
+);
+
+enum rpc_dispatch_mode
+{
+	rpc_dispatch_mode_request,
+	rpc_dispatch_mode_response,
+	rpc_dispatch_mode_reflect,
+};
+
+bool rpc_dispatch_with_mode
+(
+	struct rpc_node*,
+	struct rpc_request*,
+	enum rpc_dispatch_mode
+);
 
 #endif
